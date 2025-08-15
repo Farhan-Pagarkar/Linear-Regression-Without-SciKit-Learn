26afc864f502fb5f8ff2dab0511a9da6de1545a7
Farhan-Pagarkar <farhanpagarkar0917@gmail.com>
2025-08-16 02:52:51 +0530

    linear regression updates

---
+++ LR from Scratch.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("linear_regression_data.csv")
+
+def loss_function(m,b,points):
+    total_error = 0
+    for i in range(len(points)):
+        x = points[i][0].x
+        y = points[i][1].y
+        total_error += (y - (m * x +b))**2
+    total_error /float(len(points))
+
+
+def gradient_descent(m_now,b_now,points,L):
+    m_gradient = 0
+    b_gradient = 0
+    n = len(points)
+
+    for i in range(n):
+        x = points.iloc[i].x
+        y = points.iloc[i].y
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    
+    m = m_now - m_gradient * L
+    b = b_now - b_gradient * L
+    return m,b
+
+# Define the points
+m = 0
+b = 0
+L = 0.0001
+Epochs = 250
+for i in range(Epochs):
+    if i % 50 == 0:
+        print(f'Epoch {i}')
+    m,b = gradient_descent(m,b,data,L)
+print(m,b)
+
+plt.scatter(data.x, data.y, color = "Red")
+plt.plot(list(range(20,120)), [m * x + b for x in range(20,120)], color = "black")
+plt.show()
