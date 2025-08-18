98904b82432ff960c20b16f88c1c7f9db4bc0e46
Farhan-Pagarkar <farhanpagarkar0917@gmail.com>
2025-08-18 22:06:21 +0530

    universal cvhanges

--- LR from Scratch.py
+++ LR from Scratch.py
@@ -1,32 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("linear_regression_data.csv")
+def linear_regression(file_path, x_col=None, y_col=None, L=0.0001, Epochs=250):
 
-def gradient_descent(m_now,b_now,points,L):
-    m_gradient = 0
-    b_gradient = 0
-    n = len(points)
-
-    for i in range(n):
-        x = points.iloc[i].x
-        y = points.iloc[i].y
-        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
-        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    data = pd.read_csv(file_path)
     
-    m = m_now - m_gradient * L
-    b = b_now - b_gradient * L
-    return m,b
-m = 0
-b = 0
-L = 0.0001
-Epochs = 250
-for i in range(Epochs):
-    if i % 50 == 0:
-        print(f'Epoch {i}')
-    m,b = gradient_descent(m,b,data,L)
-print(m,b)
+ 
+    if x_col is None or y_col is None:
+        x_col, y_col = data.columns[0], data.columns[1]
 
-plt.scatter(data.x, data.y, color = "Red")
-plt.plot(list(range(20,120)), [m * x + b for x in range(20,120)], color = "black")
-plt.show()
+    def gradient_descent(m_now, b_now, points, L):
+        m_gradient = b_gradient = 0
+        n = len(points)
+
+        for i in range(n):
+            x = points.iloc[i][x_col]
+            y = points.iloc[i][y_col]
+            m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
+            b_gradient += -(2/n) * (y - (m_now * x + b_now))
+
+        m = m_now - m_gradient * L
+        b = b_now - b_gradient * L
+        return m, b
+
+
+    m, b = 0, 0
+    for _ in range(Epochs):
+        m, b = gradient_descent(m, b, data, L)
+
+    print(f"Equation: y = {m:.4f}x + {b:.4f}")
+
+    # Plot
+    plt.scatter(data[x_col], data[y_col], color="red")
+    plt.plot(data[x_col], m * data[x_col] + b, color="black")
+    plt.xlabel(x_col)
+    plt.ylabel(y_col)
+    plt.show()
+
+    return m, b
